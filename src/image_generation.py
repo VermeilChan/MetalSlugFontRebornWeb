41366a735ddee982c17e9ec61ba035b0d05f0f0e
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-06-22 18:17:12 +0100

    Replace os.path with pathlib
    - Replaced `os.path` with `pathlib.Path`.
    - Simplified `generate_filename`, `generate_filename` functions.

--- src/image_generation.py
+++ src/image_generation.py
@@ -1,21 +1,16 @@
 from uuid import uuid4
-from os import path, makedirs
+from pathlib import Path
 from PIL import Image
 from special_characters import special_characters
 
 
 def generate_filename():
-    unique_id = uuid4().hex
-    return f"{unique_id}.png"
+    return f"{uuid4().hex}.png"
 
 
 def get_font_paths(font, color):
-    base_path = path.join(
-        "src", "static", "assets", "fonts", f"font-{font}", f"ms-{color}"
-    )
-    return [
-        path.join(base_path, folder) for folder in ("letters", "numbers", "symbols")
-    ]
+    base_path = Path("src") / "static" / ("assets") / "fonts" / f"font-{font}" / f"ms-{color}"
+    return [base_path / folder for folder in ("letters", "numbers", "symbols")]
 
 
 def get_character_image_path(character, font_paths):
@@ -24,21 +19,13 @@ def get_character_image_path(character, font_paths):
     if character.isspace():
         return None
     elif character.islower():
-        character_image_path = path.join(
-            characters_folder, "lower-case", f"{character}.png"
-        )
+        return characters_folder / "lower-case" / f"{character}.png"
     elif character.isupper():
-        character_image_path = path.join(
-            characters_folder, "upper-case", f"{character}.png"
-        )
+        return characters_folder / "upper-case" / f"{character}.png"
     elif character.isdigit():
-        character_image_path = path.join(numbers_folder, f"{character}.png")
+        return numbers_folder / f"{character}.png"
     else:
-        character_image_path = path.join(
-            symbols_folder, f"{special_characters.get(character, '')}.png"
-        )
-
-    return character_image_path
+        return symbols_folder / f"{special_characters.get(character, '')}.png"
 
 
 def get_character_image(character, font_paths):
@@ -46,7 +33,7 @@ def get_character_image(character, font_paths):
         return Image.new("RGBA", (30, 1), (0, 0, 0, 0))
 
     character_image_path = get_character_image_path(character, font_paths)
-    if not character_image_path or not path.isfile(character_image_path):
+    if not character_image_path or not character_image_path.is_file():
         raise FileNotFoundError(
             f"The character '{character}' is not supported, please check the supported characters "
         )
@@ -54,7 +41,7 @@ def get_character_image(character, font_paths):
     return Image.open(character_image_path)
 
 
-def generate_image(text, image_filename, font_paths):
+def generate_image(text, filename, font_paths):
     font_images = {c: get_character_image(c, font_paths) for c in set(text)}
 
     total_width = sum(font_images[c].width for c in text)
@@ -69,12 +56,12 @@ def generate_image(text, image_filename, font_paths):
         final_image.paste(character_image, (x_position, y_position))
         x_position += character_image.width
 
-    image_directory = path.join("src", "static", "generated-images")
-    makedirs(image_directory, exist_ok=True)
+    image_directory = Path("src") / "static" / "generated-images"
+    image_directory.mkdir(parents=True, exist_ok=True)
 
-    image_path = path.join(image_directory, image_filename)
+    image_path = image_directory / filename
     final_image.save(image_path, optimize=True)
 
-    image_url = f"static/generated-images/{image_filename}"
+    image_url = f"static/generated-images/{filename}"
 
     return image_url, None
